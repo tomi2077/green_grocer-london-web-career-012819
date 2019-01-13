47a7f8d526e8ac9101dafc6a660ac0a7699c85ed
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-13 00:12:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,7 @@ def consolidate_cart(cart)
   new_hash = {}
   cart.each do array
     array.each do |fruit, infos|
-    new_hash[fruit] << infos
+    new_hash[fruit] = infos
     new_hash[fruit][infos][:count] = fruit.count
   end
   new_hash
